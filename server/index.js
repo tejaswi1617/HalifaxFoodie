dfc66257be9826240de1ec544e5a2cd0c29d10ea
Tejaswi Rameshkumar Chaudhary <tj754396@dal.ca>
2021-07-27 14:19:37 -0300

    commiting word cloud code

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ app.use(express.json())
 //app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use("/pubsub", pubsubRoute)
+app.use("/wordcloud", wordcloudRoute)
 
 // app.get("*", (req, res) => {
 //     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
